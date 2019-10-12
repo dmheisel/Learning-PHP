ca12185ae8ed485d15a2d2780e92c39f4fe9f8e9
dmheisel <d.m.heisel@gmail.com>
2019-10-12 11:03:41 -0500

    initial commit

---
+++ HelloWorld.php
@@ -0,0 +1,10 @@
+<?php
+  $user = 'David';
+  echo "Hello $user!";
+  $currentYear = 2019;
+  $birthYear = 1984;
+  $age = $currentYear - $birthYear;
+  echo "\n";
+  echo $age;
+  echo "\n$user is $age years old!"
+?>
---
+++ readMe.md
@@ -0,0 +1,9 @@
+# Learning PHP - My Notes
+
+---
+
+## Basic Language Syntax:
+
+- PHP is often included directly in HTML files, and declared with a php tag like `<?php ...your code here ?>`
+
+
